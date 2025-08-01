db7fcb399574e5fc45580b0675765f369da85e2c
Lauti <Lauti@DESKTOP-SUMD7P2>
2025-08-01 01:41:13 -0300

    Se agrego estructura y estilos base para secciones AboutMe y MyPortfolio

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./Pages/Home/Navbar";
 import Home from "./Pages/Home/HomeScreen";
 
+
+
 function App() {
   return (
     <div className="App">
---
+++ src/Pages/Home/MyPortfolio.jsx
@@ -0,0 +1,72 @@
+import data from "../../Data/index.json";
+
+export default function MyPortfolio() {
+  return (
+    <section className="portfolio--section" id="MyPortfolio">
+      <div className="portfolio--container-box">
+        <div className="portfolio--container">
+          <p className="sub--title">Recent Projects</p>
+          <h2 className="section--heading">My Portfolio</h2>
+        </div>
+        <div>
+          <button className="btn btn-primary">
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="20"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                style={{ marginRight: "8px" }}
+            >
+                <path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M12 0C5.37 0 0 5.37 0 12c0 5.3 3.438 9.8 8.207 11.387.6.113.793-.26.793-.577
+                0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.612-4.042-1.612-.546-1.387-1.333-1.756-1.333-1.756-1.09-.745.083-.729.083-.729
+                1.205.084 1.84 1.236 1.84 1.236 1.07 1.832 2.809 1.303 3.495.997.108-.775.418-1.303.76-1.603-2.665-.305-5.467-1.333-5.467-5.93
+                0-1.31.468-2.382 1.236-3.222-.124-.303-.536-1.523.117-3.176 0 0 1.008-.322 3.3 1.23a11.5 11.5 0 013.003-.403
+                c1.018.005 2.042.138 3.003.403 2.29-1.552 3.297-1.23 3.297-1.23.655 1.653.243 2.873.12 3.176.77.84
+                1.235 1.912 1.235 3.222 0 4.61-2.807 5.624-5.48 5.92.43.37.823 1.102.823 2.222 0 1.606-.015 2.896-.015 3.286
+                0 .319.192.694.8.576C20.565 21.796 24 17.297 24 12c0-6.63-5.37-12-12-12z"
+                />
+            </svg>
+            Visit My GitHub
+            </button>
+        </div>
+      </div>
+      <div className="portfolio--section--container">
+        {data?.portfolio?.map((item, index) => (
+          <div key={index} className="portfolio--section--card">
+            <div className="portfolio--section--img">
+              <img src={item.src} alt="Placeholder" />
+            </div>
+            <div className="portfolio--section--card--content">
+              <div>
+                <h3 className="portfolio--section--title">{item.title}</h3>
+                <p className="text-md">{item.description}</p>
+              </div>
+              <p className="text-sm portfolio--link">
+                {item.link}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  viewBox="0 0 20 19"
+                  fill="none"
+                >
+                  <path
+                    d="M4.66667 1.66675H18V15.0001M18 1.66675L2 17.6667L18 1.66675Z"
+                    stroke="currentColor"
+                    stroke-width="2.66667"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                </svg>
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+}
